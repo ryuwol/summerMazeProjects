a783c065359359c1f8c8b6202ea3e8bd2ff32ff3
Fsahn <1203youngjin@naver.com>
2024-07-15 16:43:43 +0900

    123
    123

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,4 +20,8 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3(Leftright, Updown, 0).normalized * speed;
         transform.Translate(movement * Time.deltaTime);
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Ãæµ¹");
+    }
 }
